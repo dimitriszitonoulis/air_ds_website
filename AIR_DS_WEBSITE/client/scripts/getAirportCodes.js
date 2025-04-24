e8a1036d791d3f107be767083f5e328d760f6fd4
Dim <188760755+Dim607@users.noreply.github.com>
2025-04-24 15:16:26 +0300

    Modififed code so that the select element for aiport codes has an optio nwith value --. Added documentation

--- AIR_DS_WEBSITE/client/scripts/getAirportCodes.js
+++ AIR_DS_WEBSITE/client/scripts/getAirportCodes.js
@@ -1,4 +1,5 @@
 async function getAirportCodes() {
+    // fetch airport codes from db
     const uri = "./server/database/services/get_airport_codes.php";
     let airportCodes = "";
     try {
@@ -11,9 +12,18 @@ async function getAirportCodes() {
         console.error(error);
         return airportCodes;
     }
+    
     // get the select elements for the departure and destination airports
     const selectElements = document.getElementsByClassName('airport-code-selection');
     
+    // add an option element with value "--"
+    for (selectElement of selectElements){
+        let option = document.createElement('option');
+        option.value = "-";
+        option.innerText = "-";
+        selectElement.appendChild(option.cloneNode(true));
+    }
+
     // add the airport codes to the select elements
     for (airportCode of airportCodes) {
         // create the option element
@@ -22,7 +32,7 @@ async function getAirportCodes() {
         option.innerHTML = airportCode;
         // append option to the select elements
         for (selectElement of selectElements) {
-            // if cloneNode is not used the option gets appended only to the 2nd select element
+            // if cloneNode is not used the <option> gets appended only to the 2nd select element
             selectElement.appendChild(option.cloneNode(true));
         }
     }
