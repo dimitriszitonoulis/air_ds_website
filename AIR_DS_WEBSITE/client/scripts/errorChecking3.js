fe4cd105e05fd3e0c2b755108eafaba5571518c2
Dim <188760755+Dim607@users.noreply.github.com>
2025-04-25 13:13:04 +0300

    added areAirportsValid() function to check for airport validity condifgured checkAiport codesto perform both real time and submit time checking

--- AIR_DS_WEBSITE/client/scripts/errorChecking3.js
+++ AIR_DS_WEBSITE/client/scripts/errorChecking3.js
@@ -12,7 +12,7 @@ function errorChecking() {
     checkDate();
 }
 
-function checkDate(){
+function checkDate() {
     const tripDate = document.getElementById('departure-date');
     const errMessageDiv = document.getElementById('departure-date-error-message');
 
@@ -30,10 +30,10 @@ function checkDate(){
 
         // if there is less than 1 minute between the departure time and now
         if (difference < minuteInMilliseconds) {
-            showError(errMessageDiv,"You can not buy tickets 1 min before the designated takeoff")
+            showError(errMessageDiv, "You can not buy tickets 1 min before the designated takeoff")
         }
-        else{
-            clearError(errMessageDiv,"hidden");
+        else {
+            clearError(errMessageDiv, "hidden");
         }
     })
 }
@@ -48,8 +48,8 @@ function checkTicketNumber() {
         if (!isNumber(ticketNumber))
             showError(errMessageDiv, "you must enter a number")
         else if (ticketNumber < 1)
-           showError(errMessageDiv, "you must buy at least 1 ticket")
-        
+            showError(errMessageDiv, "you must buy at least 1 ticket")
+
         else {
             // in case user first enters text and then number
             // change the innerText to the initial value
@@ -59,35 +59,83 @@ function checkTicketNumber() {
 }
 
 
-// Checks that the codes of the departure and the destination airport are different
 function checkAirportCodes() {
     const airportElements = document.getElementsByClassName('airport-selection');
     const aiportElementsLength = airportElements.length;
-    const errMessageDivs =  [
+    const errMessageDivs = [
         document.getElementById('departure-airport-error-message'),
         document.getElementById('destination-aiport-error-message')
     ]
 
-    // add event listeners on both airport selectio elements
-    for (let i = 0; i < aiportElementsLength; i++) {
-        airportElements[i].addEventListener('change', (e) => {
-            // get the parent of the current <select> Element (fieldset)
-            let fieldset = airportElements[i].parentElement;
-
-            // there are 2 selection element for the airport codes
-            // if i = 0, then (i + 1) % aiportElementsLength = (0 + 1) % 2 = 1
-            // if i = 1, then (i + 1) % aiportElementsLength = (1 + 1) % 2 = 0
-            if (airportElements[i].value === airportElements[(i + 1) % aiportElementsLength].value) {
-                // add the warning inside the div
-                for(let j = 0; j < errMessageDivs.length; j++)
-                    showError(errMessageDivs[j], "the departure and the destination airport must be different")
-            }
-            else {
-                for(let j = 0; j < errMessageDivs.length; j++)
-                    clearError(errMessageDivs[j])
-            }
+    // add event listeners on both airport select elements
+    for (let currentAirport = 0; currentAirport < aiportElementsLength; currentAirport++) {
+        // there are 2 selection element for the airport codes
+        // if currentAiport = 0, then (currentAiport + 1) % aiportElementsLength = (0 + 1) % 2 = 1
+        // if currentAiport = 1, then (currentAiport + 1) % aiportElementsLength = (1 + 1) % 2 = 0
+        let otherAirport = (currentAirport + 1) % aiportElementsLength
+        
+        // For real time evaluation
+        airportElements[currentAirport].addEventListener('change', (e) => {
+            areAirportsValid(airportElements, currentAirport, otherAirport, errMessageDivs);
         });
     }
+    
+    // for submit time evaluation
+    return areAirportsValid(airportElements, 0, 1, errMessageDivs);
+}
+
+/**
+ * Function that checks if the 2 aiport <select> elements have valid values.
+ * Valid values for the <select> elements are:
+ *      Every value except: --
+ *      The 2 aiports must have different values
+ * 
+ * If they have the same value then an error message is shown (using the error message divs), and false is returned.
+ * Otherwise, no message error message appears and true is returned.
+ * 
+ * ATTENTION:
+ * The order of the 2 index parameters does not matter.
+ * ex:
+ * The result for:
+ *      currentAirportIndex = 0 and otherAirportIndex = 1
+ * is the same as:
+ *      currentAirportIndex = 1 and otherAirportIndex = 0
+ * 
+ * @param {object} airportElements 
+ * @param {number} currentAirportIndex - The index of the current aiport <select> element
+ * @param {*} otherAirportIndex - The index of the other aiport <select> element 
+ * @returns {boolean} - true if the 2 <select> elements have different values, otherwise false
+ */
+function areAirportsValid(airportElements, currentAirportIndex, otherAirportIndex, errMessageDivs) {
+    // save the airport values 
+    const currentAirport = airportElements[currentAirportIndex].value
+    const otherAirport = airportElements[otherAirportIndex].value
+
+    // get the parent of the current <select> Element (fieldset)
+    let fieldset = airportElements[currentAirportIndex].parentElement;
+    
+    //check if the strings are empty
+    if (!currentAirport || !otherAirport) {
+        console.log("currentAiport = " + currentAirport);
+        console.log("otherAirport = " + otherAirport);
+        return false;
+    }
+
+    // compare the value of the 2 aiport <select> elements
+    // if they are the same show error message ON BOTH
+    if (currentAirport === otherAirport) {
+        // add the warning inside the div
+        for (let j = 0; j < errMessageDivs.length; j++)
+            showError(errMessageDivs[j], "the departure and the destination airport must be different");
+        // isValid = false;    // the choice of aiports is not valid
+        return false;
+    } 
+
+    // airport elements are different
+    for (let j = 0; j < errMessageDivs.length; j++)
+        clearError(errMessageDivs[j]);
+
+    return true;
 }
 
 function showError(element, message) {
