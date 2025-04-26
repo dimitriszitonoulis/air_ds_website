1f9dfed9ae690ffe5a6a78c5e3aeabc5d65e3c2b
Dim <188760755+Dim607@users.noreply.github.com>
2025-04-26 17:34:42 +0300

    removed unnecessary section tag

--- AIR_DS_WEBSITE/index.php
+++ AIR_DS_WEBSITE/index.php
@@ -18,44 +18,41 @@ db_initialize();
 
   <?php include_once BASE_PATH . '\client\includes\header.php'?>
 
-  <section>
-    <form action="" method="post"> 
-      <!-- <form id="form"> -->
-      <fieldset>
-        <label class="airport-label" for="airport-selection">Select the departure airport</label>
-        <br>
-        <!-- gets filled with js -->
-        <select class="airport-selection" name="departure-airport"></select>
-        <div id="departure-airport-error-message" class="error-message">Empty</div>
-      </fieldset>
+  <form action="" method="post"> 
+    <!-- <form id="form"> -->
+    <fieldset>
+      <label class="airport-label" for="airport-selection">Select the departure airport</label>
+      <br>
+      <!-- gets filled with js -->
+      <select class="airport-selection" name="departure-airport"></select>
+      <div id="departure-airport-error-message" class="error-message">Empty</div>
+    </fieldset>
 
-      <fieldset>
-        <label class="airport-label" for="airport-selection">Select the destination airport</label>
-        <br>
-        <!-- gets filled with js -->
-        <select class="airport-selection" name="destination-airport"></select>
-        <div id="destination-aiport-error-message" class="error-message">Empty</div>
-      </fieldset>
+    <fieldset>
+      <label class="airport-label" for="airport-selection">Select the destination airport</label>
+      <br>
+      <!-- gets filled with js -->
+      <select class="airport-selection" name="destination-airport"></select>
+      <div id="destination-aiport-error-message" class="error-message">Empty</div>
+    </fieldset>
 
-      <fieldset>
-        <label for="departure-date">Select Departure Date</label>
-        <input type="datetime-local" id="departure-date" name="departure-date">
-        <div id="departure-date-error-message" class="error-message">Empty</div>
-      </fieldset>
+    <fieldset>
+      <label for="departure-date">Select Departure Date</label>
+      <input type="datetime-local" id="departure-date" name="departure-date">
+      <div id="departure-date-error-message" class="error-message">Empty</div>
+    </fieldset>
 
-      <fieldset>
-        <label id="ticket-number-label" for="ticket-number">Choose the number of tickets you want</label>
-        <input type="text" id="ticket-number" name="ticket-number" value="1" required>
-        <!-- <input type="number" id="ticket-number" name="ticket-number" value="1" min="1"> -->
-        <div id="ticket-number-error-message" class="error-message">Empty</div>
-      </fieldset>
+    <fieldset>
+      <label id="ticket-number-label" for="ticket-number">Choose the number of tickets you want</label>
+      <input type="text" id="ticket-number" name="ticket-number" value="1" required>
+      <!-- <input type="number" id="ticket-number" name="ticket-number" value="1" min="1"> -->
+      <div id="ticket-number-error-message" class="error-message">Empty</div>
+    </fieldset>
 
-      <fieldset id="buy-tickes-button-fieldset">
-        <input type="submit" id="buy-tickets-button" name="buy-tickets-button" value="Purchase">
-      </fieldset>
-    </form>
-
-  </section>
+    <fieldset id="buy-tickes-button-fieldset">
+      <input type="submit" id="buy-tickets-button" name="buy-tickets-button" value="Purchase">
+    </fieldset>
+  </form>
 
   <?php include_once BASE_PATH . '\client\includes\footer.php'?>
 
