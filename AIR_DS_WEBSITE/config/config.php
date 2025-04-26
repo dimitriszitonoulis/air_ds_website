c83cdc233f2717e46a4da08b754cff6dc05cdb3a
Dim <188760755+Dim607@users.noreply.github.com>
2025-04-26 23:35:13 +0300

    added new variable to be used by html pages as the start of the URLs for included .js and .css files. This way there will not be any problem with the inlcude paths

--- AIR_DS_WEBSITE/config/config.php
+++ AIR_DS_WEBSITE/config/config.php
@@ -5,5 +5,6 @@
 // define a constant to get the root folder of the project
 // BASE_PATH = AIR_DS_WEBSITE folder
 define('BASE_PATH', __DIR__ . '/../');
-
+    
+define('BASE_URL', '/WEB_ZITONOULIS_DIMITRIOS_E22054/AIR_DS_WEBSITE/client/');
 ?>
