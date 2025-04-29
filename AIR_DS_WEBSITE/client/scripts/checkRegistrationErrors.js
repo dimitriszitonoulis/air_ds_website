6cc1ee49cf52510888257df573602ea322d627f5
Dim <188760755+Dim607@users.noreply.github.com>
2025-04-29 21:38:38 +0300

    added function that checks the edb to see if a username is available

--- AIR_DS_WEBSITE/client/scripts/checkRegistrationErrors.js
+++ AIR_DS_WEBSITE/client/scripts/checkRegistrationErrors.js
@@ -24,3 +24,43 @@
  *    Maybe for the specific example the server can return the name of 
  *
  */
+
+
+
+
+
+async function isUsernameAvailable(username) {
+    // fetch from db
+    const url = `${BASE_URL}/server/database/services/db_is_username_stored.php`;
+
+    let usernames = "";
+    let data = "";
+    try{
+         // get all the usernames that match username 
+        // (are the same as username or have the pattern: <username><other_characters>)
+        const response = fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'username': `${username}`}) 
+            });
+            if(!response.ok){
+                throw new Error("HTTP error " + response.status);
+            }
+        data = await response.json();
+    } catch(error) {
+        console.error(error);
+        return false;
+    }
+
+    usernames = data.map(user =>user.username)
+    
+    // if no userames are returned the username is availble
+    if (usernames[0] === undefined) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
