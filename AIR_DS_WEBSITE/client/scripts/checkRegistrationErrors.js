647a519a454d39cb06fbe86ee1babdcb6c4e9cd5
Dim <188760755+Dim607@users.noreply.github.com>
2025-04-29 23:42:37 +0300

    Added the following functions: checkUsername() isUsernameValid() isUsernameAvailable() showError() clearError()

--- AIR_DS_WEBSITE/client/scripts/checkRegistrationErrors.js
+++ AIR_DS_WEBSITE/client/scripts/checkRegistrationErrors.js
@@ -29,6 +29,82 @@
 
 
 
+
+
+/**
+ * Function that evaluates if the username entered by the user is alright.
+ * 
+ * This function performs:
+ *  Real time evaluation, by adding an event listener on the username input element
+ *  Submit time evaluation, by returning true (username is alright) or false (otherwise)
+ * 
+ * @returns {boolean} - true if the username is alright, false otherwise
+ */
+function checkUsername() {
+    const usernameInput = document.getElementById('username-input');
+    const errMessageDiv = document.getElementById('username-input-error-message');
+
+    usernameInput.addEventListener('change', (e) => {
+        // for real time evaluation
+        isUsernameValid(usernameInput, errMessageDiv);
+    });  
+    // for submit time evaluation
+    return isUsernameValid(usernameInput, errMessageDiv);
+}
+
+/**
+ * Function that checks if the username in the userame input field is valid.
+ * 
+ * If the username is not valid then an error message is shown in the page and false is returned.
+ * 
+ * LOGIC:
+ *  - checks if the username is empty
+ *  - checks if the username is available 
+ *  
+ * @param {object} usernameInput - the input element for the username
+ * @param {object} errMessageDiv - The div used to display error messages for the username
+ * @returns {boolean} - true if the username is valid, false otherwise
+ */
+function isUsernameValid(usernameInput, errMessageDiv) {
+    let username = usernameInput.value;
+
+    // if input empty
+    if (!username) 
+        return false;
+
+    // if the username is not available
+    if (!isUsernameAvailable(username)) {
+        showError(errMessageDiv, "username is taken.");
+        return false;
+    }
+
+    clearError(errMessageDiv);
+    return true;
+}
+
+
+/**
+ * Function that checks if the username is available
+ * 
+ * A username is AVAILABLE ONLY if it does NOT already exist in the database.
+ * A username MATCHES the one entered by the user if it is the same, or if it is the same followed by other characters.
+ * 
+ * LOGIC:
+ *  This functions fetches the server to see if the username is available.
+ *  The server returns as a response an array. Each element of the array is a json, like: {username: "<username_value>"}.
+ *      
+ *      e.x.
+ *          [ {username: "D"},
+ *            {username: "Da"},  
+ *            {username: "Db"} ] 
+ * 
+ *  If the array is undefined, then the server did not find the username in the database.
+ *  If the array is not undefined, then it contains all the matching usernames in ascending order,
+ *  based on the number of characters they have.
+ * 
+ * @param {string} username - the username entered by the user 
+ * @returns {boolean} - true if the username does not exist in the database, false otherwise
+ */
 async function isUsernameAvailable(username) {
     // fetch from db
     const url = `${BASE_URL}/server/database/services/db_is_username_stored.php`;
@@ -36,7 +112,7 @@ async function isUsernameAvailable(username) {
     let usernames = "";
     let data = "";
     try{
-         // get all the usernames that match username 
+        // get all the usernames that match username 
         // (are the same as username or have the pattern: <username><other_characters>)
         const response = fetch(url, {
             method: "POST",
@@ -54,13 +130,21 @@ async function isUsernameAvailable(username) {
         return false;
     }
 
-    usernames = data.map(user =>user.username)
+    usernames = data.map(user => user.username)
     
-    // if no userames are returned the username is availble
-    if (usernames[0] === undefined) {
+    // if no usernames are returned the username is availble
+    if (usernames[0] === undefined)
         return true;
-    }
-    else {
+    else
         return false;
-    }
+}
+
+
+function showError(errMessageDiv, message) {
+    errMessageDiv.innerText = message;
+    errMessageDiv.style.visibility = "visible";
+}
+
+function clearError(errMessageDiv) {
+    errMessageDiv.style.visibility = "hidden";
 }
