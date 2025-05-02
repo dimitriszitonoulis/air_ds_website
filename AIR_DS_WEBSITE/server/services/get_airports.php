0efb9afc9fef8bdb45b01c47e2454890a3a43a7e
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-02 21:27:07 +0300

    forgot to change path in  require_once

--- AIR_DS_WEBSITE/server/services/get_airports.php
+++ AIR_DS_WEBSITE/server/services/get_airports.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/../../../config/config.php";
+require_once __DIR__ . "/../../config/config.php";
 require_once BASE_PATH . "./server/database/db_utils/db_connect.php";
 
 $conn = db_connect();
