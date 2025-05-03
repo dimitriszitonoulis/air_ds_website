18670ff3d62e7e0a1965f6797a91d41608920625
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-03 21:46:17 +0300

    changed the check_registration_errors() function so that it takes the input from js scripts using post placed all the headeres for json before the message is sent added checks for each field to see if it is null when it is sent from js in JSON added responses when the check is done that the user is (or isn't) registered (the implementation for the registration will be done later) modified the is_email_valid() function it returned false when the email was already in the database

--- AIR_DS_WEBSITE/server/services/auth/check_registration_errors.php
+++ AIR_DS_WEBSITE/server/services/auth/check_registration_errors.php
@@ -1,58 +1,68 @@
 <?php
 require_once __DIR__ . "/../../../config/config.php";
 require_once BASE_PATH . "server/database/db_utils/db_connect.php";
-require_once BASE_PATH . "server/database/services/db_is_username_stored.php"
-require_once BASE_PATH . "server/database/services/auth/db_is_email_stored.php"
+require_once BASE_PATH . "server/database/services/auth/db_is_username_stored.php";
+require_once BASE_PATH . "server/database/services/auth/db_is_email_stored.php";
+
+
+error_reporting(0);
+ini_set('display_errors', 0);
+
+
+check_registration_errors();
 
 
 
-check_authentication_errors();
+function check_registration_errors() {
+    $conn = NULL;
+    try {
+        $conn = db_connect();
+    } catch (PDOException $e) {
+        header('Content-type: application/json');
+        http_response_code(500);
+        echo json_encode(["error" => "Database connection falied"]);
+        exit;
+    }
 
-function check_authentication_errors() {
-    // $conn = NULL;
-    // try {
-    //     $conn = db_connect();
-    // } catch (PDOException $e) {
-    //     http_response_code(500);
-    //     header('Content-type: application/json');
-    //     echo json_encode(["error" => "Database connection falied"]);
-    // }
-
-    // // get the data from js script
-    // $content = trim(file_get_contents("php://input")); // trim => remove white space from beggining and end
-    // $decoded_content = json_decode($content, true); // true is used to get associative array
-    
-    // // if for some reason no data comes from the client (indiviadual array fields MUST be checked later)
-    // if(!isset($decoded_content) || empty($decoded_contentent)) {
-    //     http_response_code(400);
-    //     echo json_encode(["error" => "Missing 'username' in JSON"]);
-    //     exit;
-    // }
-
-    $decoded_content['name'] = "hello";
-    $decoded_content['surname'] = "zisd";
-    $decoded_content['username'] = "asd";
-    $decoded_content['password'] = "12345";
-    $decoded_content['email'] = "asdf@gmail.com";
+    // get the data from js script
+    $content = trim(file_get_contents("php://input")); // trim => remove white space from beggining and end
+    $decoded_content = json_decode($content, true); // true is used to get associative array
 
 
-    print_r($decoded_content);
+    // if for some reason no data comes from the client (indiviadual array fields MUST be checked later)
+    if(!isset($decoded_content) || empty($decoded_content)) {
+        header('Content-type: application/json');
+        http_response_code(400);
+        echo json_encode(["error" => "Missing content"]);
+        exit;
+    }
+
+    $fields = ["name", "surname", "username", "password", "email"];
+
+    foreach ($fields as $field) {
+        if (!isset($decoded_content[$field])) {
+            header('Content-type: application/json');
+            http_response_code(400);
+            echo json_encode(["error" => "Missing field: $field"]);
+            exit;
+        }
+    }
 
     $is_name = is_name_valid($decoded_content["name"]);
-
     $is_surname = is_name_valid($decoded_content["surname"]);
-    
-    // must implement the function later
-    $is_username_valid = is_username_valid($conn, $decoded_content["username"]);
-
-    
+    $is_username = is_username_valid($conn, $decoded_content["username"]);
     $is_password = is_password_valid($decoded_content["password"]);
-    $is_email = is_email_valid($decoded_content["email"]);
+    $is_email = is_email_valid($conn, $decoded_content["email"]);
 
-    if($is_name && $is_surname && $is_username_valid $$is_password && $is_email)
-        return true;
+    if($is_name && $is_surname && $is_username && $is_password && $is_email){
+        header('Content-Type: application/json');
+        echo json_encode(["response" => "user registered"]);
+        exit;
+    }
 
-    return false;
+    header('Content-Type: application/json');
+    echo json_encode(["response" => "failed to register user"]);     
+    exit;
 }
 
 
@@ -70,30 +80,29 @@ function is_name_valid ($name) {
 }
 
 
-// TODO write script that checks the database 
 function is_username_valid ($conn, $username) {
     if (!isset($username) || empty($username)) return false;
     if (!is_alphanumeric($username)) return false;
 
-    $result = db_is_username_stored();
-    if (!isset($result) || empty($result)) return false;
+    // if there are no usernames like $username in the db then an empty array is returned
+    $result = db_is_username_stored($conn, $username);
+
+    if (count($result) !== 0) return false;
 
     return true;
 }
 
-function isPasswordValid ($password) {
+function is_password_valid ($password) {
     if (!isset($password) || empty($password)) return false;
     if (!contains_number($password)) return false;
     if (strlen($password) < 4 || strlen($password) > 10)  return false;
     return true;
 }
 
-// TODO maybe check if email already exists
-function isEmailValid ($email) {
+function is_email_valid ($conn, $email) {
     if (!isset($email) || empty($email)) return false;
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) return false;
-    if (!db_is_email_stored($conn, $email)) return false;
-
+    if (db_is_email_stored($conn, $email)) return false;
     return true;
 }
 
