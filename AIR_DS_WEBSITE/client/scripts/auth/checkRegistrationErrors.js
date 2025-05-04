ece035c65fde610accaf9d310bd49f3640f87964
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-05 00:25:22 +0300

    created new file containing the functions responsible for real time and submit time validation

--- AIR_DS_WEBSITE/client/scripts/auth/checkRegistrationErrors.js
+++ AIR_DS_WEBSITE/client/scripts/auth/checkRegistrationErrors.js
@@ -1,5 +1,6 @@
 import { registerUser } from "./registerUser.js";
 import { isNameValid, isUsernameValid, isPasswordValid, isEmailValid } from "./fieldValidatorFunctions.js";
+import { setUpSubmitTimeValidation, setUpRealTimeValidation} from "./validatorManager.js";
 
 /**
  * TODO
@@ -65,28 +66,8 @@ const registerBtn = document.getElementById('register-button');
 registerBtn.addEventListener('click', async (e) => {
     // if the button is clicked without any field being checked do nothing
     e.preventDefault();
-
-    // assume that all fields are valid ( for now ;) )
-    let isAllValid = true;
-
-    // loop through all the fields and check if they are valid
-    for (const field of fields) {
-        const inputElement = document.getElementById(field.inputId)
-        const errorElement = document.getElementById(field.errorId);
-        const isAsync = field.isAsync;
-        let isValid = true;
-
-        if (isAsync) // if the function is async await its response
-            isValid = await field.validatorFunction(inputElement, errorElement);
-        else
-            isValid = field.validatorFunction(inputElement, errorElement);
-
-        // if the field is not valid then set that not all fields are valid
-        if (!isValid) {
-            isAllValid = false;
-            break;  // no need to check the rest of the elemenets, form won't be submitted
-        }
-    }
+   
+    let isAllValid = setUpSubmitTimeValidation(fields);
 
     // only if all the fields are valid submit the form
     if (isAllValid) {
@@ -107,34 +88,5 @@ registerBtn.addEventListener('click', async (e) => {
     }
 });
 
-
-/**
-* function responsible for the adding validation checks on the <input> field with id = inputId
-* 
-* This function performs:
-*  Real time evaluation, by adding an event listener on the <input> element
-*  Submit time evaluation, by returning true or false
-*  @param {JSON} - A JSON containing:
-* - inputId: {string} the id of the <input> element on which the validation is applied
-* - errorId: {string} the id of the <div> element containing the error message for the <input> element
-* - event: {string} the event to pass on to the eventListener
-* - validatorFunction: {function} the function that performs the validation checks
-* - isAsync: {boolean} true if the validator function is asynchronous false otherwise
-* @returns {boolean} - true if the password is alright, false otherwise
-*/
-function setUpValidation() {
-    for(const field of fields) {
-        const inputElement = document.getElementById(field.inputId);
-        const errorElement = document.getElementById(field.errorId);
-
-        inputElement.addEventListener(field.event, async (e) => {
-            if(field.isAsync) await field.validatorFunction(inputElement, errorElement);
-            else field.validatorFunction(inputElement, errorElement);
-        });
-    }
-}
-
-
-// TODO CHANGE LATER TO NOT MAKE CALL
 // Call the function
-setUpValidation();
+setUpRealTimeValidation(fields);
