519c4a348d2893cc214a19eb5b506d8c7aa11bb2
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-05 19:38:05 +0300

    Added parameter that checks if the validation is performer for register of login if it is for login chec that the password matches the corresponding username

--- AIR_DS_WEBSITE/server/api/auth/validation_manager.php
+++ AIR_DS_WEBSITE/server/api/auth/validation_manager.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/../../../config/config.php";
 require_once BASE_PATH . "server/database/db_utils/db_connect.php";
 require_once BASE_PATH . "server/api/auth/field_validator_functions.php";
-function validate_fields($conn, $decoded_content, $fields) {
+function validate_fields($conn, $decoded_content, $fields, $for_login=false) {
     // if for some reason no data comes from the client (individual array fields checked later)
     if(!isset($decoded_content) || empty($decoded_content))
        return ["result" => false, "message" => "Missing content"];
@@ -16,6 +16,14 @@ function validate_fields($conn, $decoded_content, $fields) {
     $fields["surname"] = is_name_valid($decoded_content["surname"]);
     $fields["username"] = is_username_valid($conn, $decoded_content["username"]);
     $fields["password"] = is_password_valid($decoded_content["password"]);
+
+    // TODO maybe move this to the validator_functions.php
+    // if checking for login add another check for the password
+    if ($for_login) {
+        $fields["password"] = db_is_password_correct($conn, $fields["username"], $fields["password"]);
+    }
+
+
     $fields["email"] = is_email_valid($conn, $decoded_content["email"]);
 
     foreach ($fields as $field => $isValid) {
