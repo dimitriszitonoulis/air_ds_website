d9c827a1069d2126a877271a5a9d6ca4ccb1cd9f
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-06 13:06:31 +0300

    added script that checks that fetches the db to check if a user can login

---
+++ AIR_DS_WEBSITE/client/scripts/auth/loginUser.js
@@ -0,0 +1,27 @@
+export async function loginUser(values, BASE_URL) {
+    const url = `${BASE_URL}server/api/auth/check_login_errors.php`;
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: { 'Content-Type': "application/json"},
+            body: JSON.stringify(values)
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            console.error("Server returned error", data);
+            throw new Error("HTTP error " + response.status);
+        }
+
+        console.log("Fetch succesful return data:", data)
+
+        // if the user logs in this is true, otherwise false
+        return data['result'];
+
+    } catch (error) {
+        console.error("Error fetching data: ", error);
+        return false;
+    }
+}
