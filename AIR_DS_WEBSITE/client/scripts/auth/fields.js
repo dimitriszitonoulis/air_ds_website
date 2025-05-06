daa6a448d9bf193eb0bfb90eb467d4a49445ffe7
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-06 19:21:20 +0300

    added fields.js which contains global variable with all the fields that need to be user both for registration and login

---
+++ AIR_DS_WEBSITE/client/scripts/auth/fields.js
@@ -0,0 +1,39 @@
+import { isNameValid, isUsernameValid, isPasswordValid, isEmailValid } from "./fieldValidatorFunctions.js";
+
+export const fields = {
+    'name': {
+        inputId: 'name-input',
+        errorId: 'name-input-error-message',
+        event: 'change',
+        validatorFunction: isNameValid,
+        isAsync: false
+    },
+    'surname': {
+        inputId: 'surname-input',
+        errorId: 'surname-input-error-message',
+        event: 'change',
+        validatorFunction: isNameValid,
+        isAsync: false
+    },
+   'username':  {
+        inputId: 'username-input',
+        errorId: 'username-input-error-message',
+        event: 'change',
+        validatorFunction: isUsernameValid,
+        isAsync: true
+    },
+    'password': {
+        inputId: 'password-input',
+        errorId: 'password-input-error-message',
+        event: 'change',
+        validatorFunction: isPasswordValid,
+        isAsync: false
+    },
+    'email':{
+        inputId: 'email-input',
+        errorId: 'email-input-error-message',
+        event: 'change',
+        validatorFunction: isEmailValid,
+        isAsync: false
+    }
+}
