089ddc7b2aef3eab2819f9d5067d4cf2f2977d52
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-08 12:41:52 +0300

    added check to show login when the user is not loged in and log out when the user is logged out

--- AIR_DS_WEBSITE/client/includes/header.php
+++ AIR_DS_WEBSITE/client/includes/header.php
@@ -1,8 +1,19 @@
+<?php
+require_once __DIR__ . "/../../config/config.php";
+require_once BASE_PATH . "config/config.php";
+?>
+
+<!-- TODO implement logout logic (erase session variables) -->
+
 <header id="header">
   <nav>
-  <a>Home</a>
-  <a>My Trips</a>
-  <a>Login</a>
-  <a>Logout</a>
+    <a>Home</a>
+    <a>My Trips</a>
+
+    <?php if (!isset($_SESSION["userId"])) { ?>
+      <a href="<?= BASE_URL . "client/pages/auth/login.php"?>">Login</a>
+    <?php } else { ?>
+      <a>Logout</a>
+    <?php } ?>
   </nav>
 </header>
