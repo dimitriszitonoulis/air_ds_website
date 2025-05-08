e6c2caee4185ccd2a14aa2304ccb18519a52f732
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-08 12:40:31 +0300

    added FIXME

--- AIR_DS_WEBSITE/client/includes/start_session.php
+++ AIR_DS_WEBSITE/client/includes/start_session.php
@@ -1,4 +1,10 @@
 <?php
+// FIXME
+/**
+ * it may cause problems beacause it is not in function
+ * So when I use require_once it runs only the first time
+ * If I want to run it multple times, I should put it inside a fucntion and call it everytime
+ */
 if(session_status() === PHP_SESSION_NONE)
     session_start();
 ?>
