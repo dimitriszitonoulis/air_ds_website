c7e95c0ad5a94c56c87c92a58b77d70970fe712c
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-10 16:15:36 +0300

    gave null as default value to username, if it is null throw exception

--- AIR_DS_WEBSITE/server/database/services/auth/db_is_field_stored.php
+++ AIR_DS_WEBSITE/server/database/services/auth/db_is_field_stored.php
@@ -5,7 +5,11 @@
  */
 
 // checks if the given username is stored in the database
-function db_is_username_stored($conn, $username) {
+function db_is_username_stored($conn, $username=null) {
+    // TODO maybe trhow exception????
+    // if no username is provided
+    if ($username === null) throw new InvalidArgumentException("Username must not be null.");
+    
     // query to be run
     // BINARY is used because the username is stored with collation utf8mb4_general_ci 
     // which is case insensitive
