8a371c0a91af1dc8ab6e17a2ea5f009fdbb828d9
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-10 16:44:18 +0300

    changed script to distinguish between username availability and username vaslidity

--- AIR_DS_WEBSITE/server/api/auth/is_username_stored.php
+++ AIR_DS_WEBSITE/server/api/auth/is_username_stored.php
@@ -3,17 +3,22 @@ require_once __DIR__ . "/../../../config/config.php";
 require_once BASE_PATH . "server/database/db_utils/db_connect.php";
 require_once BASE_PATH . "server/api/auth/validation_manager.php";
 require_once BASE_PATH . "server/database/services/auth/db_is_field_stored.php";
+require_once BASE_PATH . "config/messages.php";
 
 is_username_stored();
 
 function is_username_stored() {
+    header('Content-type: application/json');
+    $response_message = get_response_message([]);
+
     $conn = NULL;
     try {
         $conn = db_connect();
     } catch (PDOException $e) {
-        header('Content-type: application/json');
+       $response = $response_message['failure']['connection'];
         http_response_code(500);
-        echo json_encode(["error" => "Database connection falied"]);
+        echo json_encode($response);
+        exit;
     }
 
     // get the data from js script
@@ -32,28 +37,43 @@ function is_username_stored() {
     }
 
     $response = null;
+    // response = ["result" => boolean, "message" => string]
     $response = validate_fields($conn, $decoded_content, $fields, false);
-
-    //FIXME
-    /**
-     * problems:
-     * - I dont know if the name is invalid or if it does not exist I just get true or false
-     *  I can get that from client (based on how the cheks are made in client, username availability is the last one 
-     *  it can change)
-     * - Incorrect http response code 400 should be sent only if the username is syntactically incorrect not if it does not exist
-     * - MAybe modify the validator function to answer if the username is not valid because of syntax or availability
-     */
-
-
+    // if a field is invalid
     if (!$response["result"]) {
-        header('Content-Type: application/json');
+        http_response_code(400);
         echo json_encode($response);
         exit;
     }
 
+    $username = null;
+    // check if the username is inside the content sent by the client
+    if (array_key_exists("username", $decoded_content)) {
+        // if it is, then its value has already been validated,
+        // So no additional checks needed
+        $username = $decoded_content["username"];
+    }
+    // is the username stored?
+    try {
+        $is_username_stored = db_is_username_stored($conn, $username);
+    } catch (Exception $e) {
+        // if exception do nothing
+        $response = $response_message['failure']['nop'];
+        http_response_code(500);
+        echo json_encode($response);
+        exit;  
+    }
+
+    // if the username is stored
+    if($is_username_stored) {
+        $response = ['result' => true, 'message' => "username is stored"];
+        http_response_code(200);
+        echo json_encode($response);
+        exit;
+    }
     
     header('Content-Type: application/json');
-    echo json_encode(["result" => true, "message" => "username is not stored"]);
+    echo json_encode(["result" => false, "message" => "username is not stored"]);
     exit;
 }
 ?>
