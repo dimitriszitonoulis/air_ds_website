8f910832cc53bd0fd08bcfb80367a915962ef776
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-10 13:11:14 +0300

    modified to use tha improved validator functions

--- AIR_DS_WEBSITE/server/api/auth/check_login_errors.php
+++ AIR_DS_WEBSITE/server/api/auth/check_login_errors.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . "/../../../config/config.php";
 require_once BASE_PATH . "server/api/auth/validation_manager.php";
-require_once BASE_PATH. "server/api/auth/login_user.php";
+require_once BASE_PATH . "server/api/auth/login_user.php";
+require_once BASE_PATH . "config/messages.php";
 
 // error_reporting(0);
 // ini_set('display_errors', 0);
@@ -24,29 +25,45 @@ function check_login_errors() {
     $content = trim(file_get_contents("php://input")); // trim => remove white space from beggining and end
     $decoded_content = json_decode($content, true); // true is used to get associative array
 
-    // Array with: field names => field validity
-    $fields = [
-        "username" => false,
-        "password" => false,
-    ];
+    // what if the keys are not what I am expecting?
+    // get the name of the fields that come from the client
+    $field_names = array_keys($decoded_content);
 
-    // response = ["result" => boolean, "message" => string]
+    // array showing the validity of each field
+    // like: field name => validity (boolean)
+    // for now initialize all fields as false
+    $fields =[];
+    foreach($field_names as $name) {
+        $fields[$name] = false;
+    }
+
     $response = null;
     $response = validate_fields($conn, $decoded_content, $fields, true);
   
-    if (!$response['result']) {
-        header('Content-type: application/json');
+    if (!$response["result"]) {
+        header('Content-Type: application/json');
+        // 400 should only be returned if the input is syntactically incorrect
+        // it would not be right to send 400 if a username is taken
         http_response_code(400);
         echo json_encode($response);
         exit;
     }
- 
-    // if this point is reached all the fields are valid
-    $response = login_user($conn, $decoded_content);
 
-    // these are set from login_user()
-    // $response["result"] = true;
-    // $response["message"] = "user registered";
+    // if this point is reached all the fields are valid
+    login_user($conn, $decoded_content);
+
+
+    $response_message = get_response_message([]);
+
+    if(!isset($_SESSION['userId'])) {
+        $response = $response_message['failure']['nop'];
+        header('Content-Type: application/json');
+        // echo json_encode(['result' => true, "session" => $_SESSION['userId']]);
+        echo json_encode($response);
+        exit;  
+    }
+
+    $response = $response_message['login']['success'];
     header('Content-Type: application/json');
     echo json_encode($response);
     exit;  
