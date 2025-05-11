300f364018f36db72a30f7db102b2a39608d1b58
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-11 14:03:22 +0300

    added extra function for showing redirect messages

---
+++ AIR_DS_WEBSITE/client/scripts/messageDisplay.js
@@ -0,0 +1,39 @@
+export function showError(element, message) {
+    element.innerText = message;
+    // maybe not needed, this is done from css
+    element.style.color = "red";
+    element.style.visibility = "visible";
+}
+
+export function clearError(element) {
+    element.style.visibility = "hidden";
+}
+
+export function showMessage(element, color, message) {
+    element.innerText = message;
+    element.style.color = color;
+    element.style.visibility = "visible";
+}
+
+export function hideMessage(element) {
+    element.style.visibility = "hidden";
+}
+
+export function appendMessage(element, messageToAppend) {
+    element.innerText += messageToAppend;
+}
+
+
+export function showRedirectMessage(repeats, messageElement, appendedMessage, url, intervalTime) {
+    let countDown = repeats;
+
+    const intervalId = setInterval(() => {
+        appendMessage(messageElement, appendedMessage);
+        countDown--;
+
+        if (countDown === 0){
+            clearInterval(intervalId);
+            window.location.replace(url);
+        }
+    }, intervalTime);
+}
