bff312989f6f53299127b5ccb4b7cf1bbe554793
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-11 17:13:13 +0300

    added comments for each field

--- AIR_DS_WEBSITE/client/pages/home.php
+++ AIR_DS_WEBSITE/client/pages/home.php
@@ -10,19 +10,21 @@ db_initialize();
 
 <head>
   <!-- <link rel="stylesheet" href="./client/styles/style.css">  -->
-  <link rel="stylesheet" href="<?= BASE_URL ?>client/styles/style.css"> 
+  <link rel="stylesheet" href="<?= BASE_URL ?>client/styles/style.css">
   <link rel="stylesheet" href="<?= BASE_URL ?>client/styles/header.css">
   <link rel="stylesheet" href="<?= BASE_URL ?>client/styles/footer.css">
 </head>
 
 <body>
 
-  <?php include_once BASE_PATH . 'client/includes/header.php'?>
+  <?php include_once BASE_PATH . 'client/includes/header.php' ?>
 
   <!-- TODO maybe delete the ids from the labels -->
 
-  <form id="purchase-tickets-form" action="" method="post"> 
+  <form id="purchase-tickets-form" action="" method="post">
     <!-- <form id="form"> -->
+
+    <!-- departure aiport select field -->
     <fieldset>
       <label class="airport-label" for="airport-selection">Select the departure airport</label>
       <br>
@@ -31,6 +33,7 @@ db_initialize();
       <div id="departure-airport-error-message" class="error-message">Empty</div>
     </fieldset>
 
+    <!-- destination airport select field -->
     <fieldset>
       <label class="airport-label" for="airport-selection">Select the destination airport</label>
       <br>
@@ -39,12 +42,14 @@ db_initialize();
       <div id="destination-aiport-error-message" class="error-message">Empty</div>
     </fieldset>
 
+    <!-- departure date select field -->
     <fieldset>
       <label for="departure-date">Select Departure Date</label>
       <input type="datetime-local" id="departure-date" name="departure-date">
       <div id="departure-date-error-message" class="error-message">Empty</div>
     </fieldset>
 
+    <!-- ticket number field -->
     <fieldset>
       <label id="ticket-number-label" for="ticket-number">Choose the number of tickets you want</label>
       <input type="text" id="ticket-number" name="ticket-number" value="1" required>
@@ -57,7 +62,7 @@ db_initialize();
     </fieldset>
   </form>
 
-  <?php include_once BASE_PATH . 'client/includes/footer.php'?>
+  <?php include_once BASE_PATH . 'client/includes/footer.php' ?>
 
   <script> const BASE_URL = "<?= BASE_URL ?>";</script>
   <script src="<?= BASE_URL ?>client/scripts/getAirports.js"></script>
