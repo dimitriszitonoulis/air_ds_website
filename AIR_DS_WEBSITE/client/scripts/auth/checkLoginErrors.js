e0c289be73f26d9643d0cfc9f1759a5b46f698a2
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-11 14:17:31 +0300

    added redirect message

--- AIR_DS_WEBSITE/client/scripts/auth/checkLoginErrors.js
+++ AIR_DS_WEBSITE/client/scripts/auth/checkLoginErrors.js
@@ -2,15 +2,15 @@ import { fields } from "./fields.js";
 import { validateSubmitTime } from "./validationManager.js";
 import { loginUser } from "./loginUser.js"
 import { isPasswordValid, isUsernameValidLogin } from "./fieldValidatorFunctions.js";
-import { showError, clearError } from "../errorDisplay.js";
+import { showError, clearError, showMessage, showRedirectMessage } from "../messageDisplay.js";
 
 // take the necessary fields from fields global variable and add their validator fucntions
 // attention create shallow copy, do not modify the global variable
 const loginFields = {
-   "username": {...fields["username"], validatorFunction: isUsernameValidLogin},
-//    "username": {...fields["username"], validatorFunction: isUsernameInputValid},
-//    "password": {...fields["password"], validatorFunction: isPasswordValidLogin}
-   "password": {...fields["password"], validatorFunction: isPasswordValid}
+    "username": { ...fields["username"], validatorFunction: isUsernameValidLogin },
+    //    "username": {...fields["username"], validatorFunction: isUsernameInputValid},
+    //    "password": {...fields["password"], validatorFunction: isPasswordValidLogin}
+    "password": { ...fields["password"], validatorFunction: isPasswordValid }
 };
 
 
@@ -23,35 +23,43 @@ const loginBtnErrorDiv = document.getElementById('login-button-error-message');
 loginBtn.addEventListener('click', async (e) => {
     // if the button is clicked without any field being checked do nothing
     e.preventDefault();
-   
+
     const isAllValid = await validateSubmitTime(loginFields);
 
-    // TODO uncomment the check later
-    // if a field is invalid do nothing
     if (!isAllValid) {
-        // showError(loginBtnErrorDiv, "Invalid credentials");
-        return;
+        showError(loginBtnErrorDiv, "Invalid credentials");
+        // return;
     }
+
+    // get the values from the form elements
     const values = {};
     for (const key in loginFields) {
         const field = loginFields[key];
         const element = document.getElementById(field.inputId);
         values[element.name] = element.value;
     }
-   
+
     // TODO maybe modify to do something with error message
     const isLoggedIn = await loginUser(values, BASE_URL);
+    // let isLoggedIn = await loginUser(values, BASE_URL);
+
+    // TODO remove
+    // isLoggedIn = true;
 
     // Add code that fields are invalid if not alredy logged in 
     if (isLoggedIn) {
         clearError(loginBtnErrorDiv);
-        // TODO uncomment later
 
+        // TODO maybe redirect to my home and have a message there
+        const message = `Logged in sucessfully\n You will be redirected to the home page shortly`
+        showMessage(loginBtnErrorDiv, "white", message);
+
+        const redirectUrl = `${BASE_URL}/client/pages/home.php`
+        showRedirectMessage(loginBtnErrorDiv, ".", redirectUrl, 3, 1000);
         // redirect to login page
         // window.location.replace(`${BASE_URL}/client/pages/auth/login.php`)
 
-        // DELETE LATER
-        // window.location.href = `${BASE_URL}/client/pages/auth/login.php`;  
+
     } else {
         showError(loginBtnErrorDiv, "Invalid credentials")
         // console.error("registration failed")
