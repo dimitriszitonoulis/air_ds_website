aa3b6b9a83ccfdf17941c4504dae7bf969559e82
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-13 18:11:45 +0300

    placed form inside main tag and added h2 Register title

--- AIR_DS_WEBSITE/client/pages/auth/register.php
+++ AIR_DS_WEBSITE/client/pages/auth/register.php
@@ -16,50 +16,54 @@ require_once BASE_PATH . "client\includes\start_session.php";
 
 <body>
 
-<?php include BASE_PATH . "client/includes/header.php"; ?>
+  <?php include BASE_PATH . "client/includes/header.php"; ?>
 
-  <!-- <form id="registration-form" action="<?= BASE_URL ?>/server/services/auth/check_registration_errors.php"> -->
-  <form id="registration-form">
-    <!-- name field -->
-    <fieldset>
-      <label for="name-input">Name</label>
-      <input type="text" id="name-input" name="name" required>
-      <div id="name-input-error-message" class="error-message">Empty</div>
-    </fieldset>
+  <main>
 
-    <!-- surname field -->
-    <fieldset>
-      <label for="surname-input">Surname</label>
-      <input type="text" id="surname-input" name="surname" required>
-      <div id="surname-input-error-message" class="error-message">Empty</div>
-    </fieldset>
+    <h2> Register </h2>
 
-    <!-- username field -->
-    <fieldset>
-      <label for="username-input">Username</label>
-      <input type="text" id="username-input" name="username" required>
-      <div id="username-input-error-message" class="error-message">Empty</div>
-    </fieldset>
+    <form id="registration-form">
+      <!-- name field -->
+      <fieldset>
+        <label for="name-input">Name</label>
+        <input type="text" id="name-input" name="name" required>
+        <div id="name-input-error-message" class="error-message">Empty</div>
+      </fieldset>
 
-    <!-- password field -->
-    <fieldset>
-      <label for="password-input">Password</label>
-      <input type="password" id="password-input" name="password" required>
-      <div id="password-input-error-message" class="error-message">Empty</div>
-    </fieldset>
+      <!-- surname field -->
+      <fieldset>
+        <label for="surname-input">Surname</label>
+        <input type="text" id="surname-input" name="surname" required>
+        <div id="surname-input-error-message" class="error-message">Empty</div>
+      </fieldset>
 
-    <!-- email field -->
-    <fieldset>
-      <label for="email-input">Email</label>
-      <input type="email" id="email-input" name="email" required>
-      <div id="email-input-error-message" class="error-message">Empty</div>
-    </fieldset>
+      <!-- username field -->
+      <fieldset>
+        <label for="username-input">Username</label>
+        <input type="text" id="username-input" name="username" required>
+        <div id="username-input-error-message" class="error-message">Empty</div>
+      </fieldset>
 
-    <fieldset id="register-button-fieldset">
-      <input type="submit" id="register-button" name="register-button" value="Register">
-      <div id="registration-button-error-message" class="error-message"></div>
-    </fieldset>
-  </form>
+      <!-- password field -->
+      <fieldset>
+        <label for="password-input">Password</label>
+        <input type="password" id="password-input" name="password" required>
+        <div id="password-input-error-message" class="error-message">Empty</div>
+      </fieldset>
+
+      <!-- email field -->
+      <fieldset>
+        <label for="email-input">Email</label>
+        <input type="email" id="email-input" name="email" required>
+        <div id="email-input-error-message" class="error-message">Empty</div>
+      </fieldset>
+
+      <fieldset id="register-button-fieldset">
+        <input type="submit" id="register-button" name="register-button" value="Register">
+        <div id="registration-button-error-message" class="error-message"></div>
+      </fieldset>
+    </form>
+  </main>
 
   <?php include_once BASE_PATH . "client/includes/footer.php" ?>
 
