758b0140b944fd208a69b78fc33f4397aee0a78c
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-13 18:11:21 +0300

    placed form inside main tag

--- AIR_DS_WEBSITE/client/pages/auth/login.php
+++ AIR_DS_WEBSITE/client/pages/auth/login.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 require_once __DIR__ . "/../../../config/config.php";
 require_once BASE_PATH . "client/includes/start_session.php"
-?>
+  ?>
 
 <!DOCTYPE html>
 
@@ -10,46 +10,48 @@ require_once BASE_PATH . "client/includes/start_session.php"
 <head>
   <link rel="stylesheet" href="<?= BASE_URL ?>client/styles/style.css">
   <link rel="stylesheet" href="<?= BASE_URL ?>client/styles/header.css">
-  <link rel="stylesheet" href = "<?= BASE_URL ?>client/styles/form.css">
+  <link rel="stylesheet" href="<?= BASE_URL ?>client/styles/form.css">
   <link rel="stylesheet" href="<?= BASE_URL ?>client/styles/footer.css">
 </head>
 
 <body>
 
   <?php include_once BASE_PATH . "client/includes/header.php"; ?>
+
+  <main>
+
+    <h2>Login/Register</h2>
+
+    <form action="">
+      <!-- username field -->
+      <fieldset>
+        <label for="username-input">Username</label>
+        <input type="text" id="username-input" name="username" required>
+        <div id="username-input-error-message" class="error-message">Empty</div>
+      </fieldset>
+
+      <!-- password field -->
+      <fieldset>
+        <label for="password-input">Password</label>
+        <input type="text" id="password-input" name="password" required>
+        <div id="password-input-error-message" class="error-message">Empty</div>
+      </fieldset>
+
+      <fieldset id="login-button-fieldset">
+        <input type="submit" id="login-button" name="login-button" value="Login">
+        <div id="login-button-error-message" class="error-message">Empty</div>
+      </fieldset>
+    </form>
+
+    <!-- Redirects to registration form -->
+    <p>Need an account? <a href="./register.php">Register</a> </p>
   
-  
-  <h2>Login/Register</h2>
-
-  <form action="">
-    <!-- username field -->
-    <fieldset>
-      <label for="username-input">Username</label>
-      <input type="text" id="username-input" name="username" required>
-      <div id="username-input-error-message" class="error-message">Empty</div>
-    </fieldset>
-
-    <!-- password field -->
-    <fieldset>
-      <label for="password-input">Password</label>
-      <input type="text" id="password-input" name="password" required>
-      <div id="password-input-error-message" class="error-message">Empty</div>
-    </fieldset>
-
-    <fieldset id="login-button-fieldset">
-      <input type="submit" id="login-button" name="login-button" value="Login">
-      <div id="login-button-error-message" class="error-message">Empty</div>
-    </fieldset>
-  </form>
-
-  <!-- Redirects to registration form -->
-  <p>Need an account? <a href="./register.php">Register</a> </p>
-
+  </main>
 
   <?php include_once BASE_PATH . "client/includes/footer.php"; ?>
 
-<script>const BASE_URL = "<?= BASE_URL ?>";</script>
-<script type="module" src="<?= BASE_URL ?>client/scripts/auth/checkLoginErrors.js"></script>
+  <script>const BASE_URL = "<?= BASE_URL ?>";</script>
+  <script type="module" src="<?= BASE_URL ?>client/scripts/auth/checkLoginErrors.js"></script>
 </body>
 
 </html>
