2a18866bfc89644330792b97f2df61b46aa42054
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-13 18:13:26 +0300

    added main tag

--- AIR_DS_WEBSITE/client/pages/home.php
+++ AIR_DS_WEBSITE/client/pages/home.php
@@ -22,49 +22,54 @@ db_initialize();
 
   <?php include_once BASE_PATH . 'client/includes/header.php' ?>
 
-  <!-- TODO maybe delete the ids from the labels -->
+  <main>
 
-  <form id="purchase-tickets-form" action="" method="post">
-    <!-- <form id="form"> -->
 
-    <!-- departure aiport select field -->
-    <fieldset>
-      <label class="airport-label" for="departure-airport-input">Select the departure airport</label>
-      <br>
-      <!-- gets filled with js -->
-      <select id="departure-airport-input" class="airport-selection" name="departure-airport"></select>
-      <div id="departure-airport-error-message" class="error-message">Empty</div>
-    </fieldset>
+    <!-- TODO maybe delete the ids from the labels -->
 
-    <!-- destination airport select field -->
-    <fieldset>
-      <label class="airport-label" for="destination-airport-input">Select the destination airport</label>
-      <br>
-      <!-- gets filled with js -->
-      <select id="destination-airport-input" class="airport-selection" name="destination-airport"></select>
-      <div id="destination-aiport-error-message" class="error-message">Empty</div>
-    </fieldset>
+    <form id="purchase-tickets-form" action="" method="post">
+      <!-- <form id="form"> -->
 
-    <!-- departure date select field -->
-    <fieldset>
-      <label for="date-input">Select Departure Date</label>
-      <input type="datetime-local" id="date-input" name="date">
-      <div id="date-error-message" class="error-message">Empty</div>
-    </fieldset>
+      <!-- departure aiport select field -->
+      <fieldset>
+        <label class="airport-label" for="departure-airport-input">Select the departure airport</label>
+        <br>
+        <!-- gets filled with js -->
+        <select id="departure-airport-input" class="airport-selection" name="departure-airport"></select>
+        <div id="departure-airport-error-message" class="error-message">Empty</div>
+      </fieldset>
 
-    <!-- ticket number field -->
-    <fieldset>
-      <label id="ticket-label" for="ticket-input">Choose the number of tickets you want</label>
-      <input type="text" id="ticket-input" name="ticket" value="1" required>
-      <!-- <input type="number" id="ticket-number" name="ticket-number" value="1" min="1"> -->
-      <div id="ticket-error-message" class="error-message">Empty</div>
-    </fieldset>
+      <!-- destination airport select field -->
+      <fieldset>
+        <label class="airport-label" for="destination-airport-input">Select the destination airport</label>
+        <br>
+        <!-- gets filled with js -->
+        <select id="destination-airport-input" class="airport-selection" name="destination-airport"></select>
+        <div id="destination-aiport-error-message" class="error-message">Empty</div>
+      </fieldset>
 
-    <fieldset id="purchase-button-fieldset">
-      <input type="submit" id="purchase-button" name="purchase-button" value="Purchase">
-      <div id="purchase-button-error-message" class="error-message"></div>
-    </fieldset>
-  </form>
+      <!-- departure date select field -->
+      <fieldset>
+        <label for="date-input">Select Departure Date</label>
+        <input type="datetime-local" id="date-input" name="date">
+        <div id="date-error-message" class="error-message">Empty</div>
+      </fieldset>
+
+      <!-- ticket number field -->
+      <fieldset>
+        <label id="ticket-label" for="ticket-input">Choose the number of tickets you want</label>
+        <input type="text" id="ticket-input" name="ticket" value="1" required>
+        <!-- <input type="number" id="ticket-number" name="ticket-number" value="1" min="1"> -->
+        <div id="ticket-error-message" class="error-message">Empty</div>
+      </fieldset>
+
+      <fieldset id="purchase-button-fieldset">
+        <input type="submit" id="purchase-button" name="purchase-button" value="Purchase">
+        <div id="purchase-button-error-message" class="error-message"></div>
+      </fieldset>
+    </form>
+
+  </main>
 
   <?php include_once BASE_PATH . 'client/includes/footer.php' ?>
 
