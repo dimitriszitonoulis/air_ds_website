1b9a4f88fc31f74ce08d5c8ef7e1956a4d1a14be
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-14 19:24:38 +0300

    dest_code paramter had default value null instead of null

--- AIR_DS_WEBSITE/server/database/services/reservations/db_get_flight_dates.php
+++ AIR_DS_WEBSITE/server/database/services/reservations/db_get_flight_dates.php
@@ -1,6 +1,6 @@
 <?php
 
-function db_get_flight_dates($conn, $dep_code=null, $dest_code="null") {
+function db_get_flight_dates($conn, $dep_code=null, $dest_code=null) {
     // if no departure airport code is provided
     if ($dep_code === null) throw new InvalidArgumentException("Departure airport code must not be null.");
 
@@ -17,8 +17,8 @@ function db_get_flight_dates($conn, $dep_code=null, $dest_code="null") {
     $stmt = $conn->prepare($query);
 
     // Bind parameters
-    $stmt->bind(':dep_code', $dep_code, PDO::PARAM_STR);
-    $stmt->bind(':dest_code', $dest_code, PDO::PARAM_STR);
+    $stmt->bindParam(':dep_code', $dep_code, PDO::PARAM_STR);
+    $stmt->bindParam(':dest_code', $dest_code, PDO::PARAM_STR);
 
     // execute the statement
     $stmt->execute();
