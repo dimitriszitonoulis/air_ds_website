074416fc4a829568fa505e80d648efb316e16a50
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-14 18:10:24 +0300

    added script the queries the db for a specifi flights dates

---
+++ AIR_DS_WEBSITE/server/database/services/reservations/db_get_flight_dates.php
@@ -0,0 +1,30 @@
+<?php
+
+function db_get_flight_dates($conn, $dep_code=null, $dest_code="null") {
+    // if no departure airport code is provided
+    if ($dep_code === null) throw new InvalidArgumentException("Departure airport code must not be null.");
+
+    // if no destination airport code is provided
+    if ($dest_code=== null) throw new InvalidArgumentException("Departure airport code must not be null.");
+
+
+    $query = "  SELECT date
+                FROM flights
+                WHERE departure_airport = :dep_code AND destination_airport = :dest_code;
+             ";
+
+    // prepare statement
+    $stmt = $conn->prepare($query);
+
+    // Bind parameters
+    $stmt->bind(':dep_code', $dep_code, PDO::PARAM_STR);
+    $stmt->bind(':dest_code', $dest_code, PDO::PARAM_STR);
+
+    // execute the statement
+    $stmt->execute();
+
+    // get all the dates for flights between the 2 specified airports
+    $result = $stmt->fetchALL(PDO::FETCH_ASSOC);
+}
+
+?>
