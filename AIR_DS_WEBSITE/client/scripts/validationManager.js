de8c7b03f26f0e328a9ce89644cea4bc85edaacd
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-14 13:42:53 +0300

    export getCollection

--- AIR_DS_WEBSITE/client/scripts/validationManager.js
+++ AIR_DS_WEBSITE/client/scripts/validationManager.js
@@ -112,7 +112,7 @@ function applyEventListeners(inputElements, errorElements, event, isAsync, valid
  * @param {Array} ids - an array containing ids as strings  
  * @returns {Array} - an array containing all the dom elements that have the ids from the parameter array
  */
-function getCollection(ids) {
+export function getCollection(ids) {
     const elements = [];
     for (const current in ids) {
         const id = ids[current];
