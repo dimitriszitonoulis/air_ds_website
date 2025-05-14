3f4d83ea1a6722bdb3a2782d9f0a1bc4d1f03a84
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-14 18:22:27 +0300

    remove commented out code

--- AIR_DS_WEBSITE/server/database/services/auth/db_is_field_stored.php
+++ AIR_DS_WEBSITE/server/database/services/auth/db_is_field_stored.php
@@ -35,36 +35,7 @@ function db_is_username_stored($conn, $username=null) {
 
     return true;
 }
-// checks if the password is correct for the given username
-// function db_is_password_correct($conn, $username, $password) {
-//     // query to be run
-//     // BINARY is used because the username is stored with collation utf8mb4_general_ci 
-//     // which is case insensitive
-//     // either use BINARY or use collation utf8mb4_bin when creating the db
-//     $query = "  SELECT username, password
-//                 FROM users 
-//                 WHERE BINARY username LIKE :username AND password LIKE :password;
-//             ";
-    
-//     // prepare the statement
-//     $stmt = $conn->prepare($query);
 
-//     // Bind parameters
-//     $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-//     $stmt->bindParam(':password', $password, PDO::PARAM_STR);
-
-//     // execute the statement
-//     $stmt->execute();
-
-//     // get the all the passwords that match for the given username 
-//     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-//     // if there are no rows then there is no acount with the given username,
-//     // that has the given password
-//     if (count($result) === 0) return false;
-
-//     return true;
-// }
 function db_is_email_stored($conn, $email) {
     // query to be run
     // BINARY is used because the email is stored with collation utf8mb4_general_ci 
