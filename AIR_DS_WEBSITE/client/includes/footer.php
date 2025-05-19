d814a769104a0f00b5035db39993e1205c61d386
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-19 23:42:12 +0300

    added elements to the footer

--- AIR_DS_WEBSITE/client/includes/footer.php
+++ AIR_DS_WEBSITE/client/includes/footer.php
@@ -1,3 +1,13 @@
 <footer id="footer">
   We wish you a happy trip!
+
+  <div id="about-us">
+    <a href="tel:+302102103330">mobile number</a>
+    <a href="mailto:air_ds@unipi.gr">email</a>
+    Copyright &copy; 2025 Air Ds. All Rights Reserved
+  </div>
+  <div id="map">
+    A map
+  </div>
+
 </footer>
