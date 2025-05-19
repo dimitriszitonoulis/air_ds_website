13de4beeb494516a2c6203478cf3aa7d3dcbd2ca
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-19 18:57:35 +0300

    added code to see if a seat is stored for a specific flight

--- AIR_DS_WEBSITE/server/database/services/db_is_field_stored.php
+++ AIR_DS_WEBSITE/server/database/services/db_is_field_stored.php
@@ -93,4 +93,70 @@ function db_is_airport_code_stored($conn, $code=null) {
 
     return true;
 }
+
+//TODO has similar code as db_get_taken_seats in db_get_flight_dates.php
+// if something is wrong here it is also propaly wrong there
+function db_is_seat_stored($conn, $seat, $dep_code, $dest_code, $dep_date){
+    // if no seat is provided
+    if ($seat === null) 
+        throw new InvalidArgumentException("Departure airport code must not be null.");
+
+    // if no departure airport code is provided
+    if ($dep_code === null)
+        throw new InvalidArgumentException("Departure airport code must not be null.");
+
+    // if no destination airport code is provided
+    if ($dest_code === null)
+        throw new InvalidArgumentException("Departure airport code must not be null.");
+
+    // if the departure date is null
+    if ($dep_date === null)
+        throw new InvalidArgumentException("Departure date must not be null.");
+
+
+    // find the flight id
+    $query =
+    "   SELECT
+            id
+        FROM 
+            flights
+        WHERE 
+            departure_airport = ':dep_code' AND destination_airport = ':dest_code' AND date = ':dep_date';
+    ";
+    // prepare statement
+    $stmt = $conn->prepare($query);
+    // bind parameters
+    $stmt->bindParam(':dep_code', $dep_code, PDO::PARAM_STR);
+    $stmt->bindParam(':dest_code', $dest_code, PDO::PARAM_STR);
+    $stmt->bindParam(':dep_date', $dep_date, PDO::PARAM_STR);
+    // execute query
+    $stmt->execute();
+
+    // retrieve response (1 row with the flight id)
+    $id_array = $stmt->fetchAll(PDO::FETCH_NUM);
+    // TODO check if it needs another [0]
+    $flight_id = $id_array[0]; // extract the id
+
+    // find the taken seats from 
+    $query =
+    "   SELECT
+            seat
+        FROM 
+            reservations
+        WHERE
+            flight_id = ':flight_id' AND seat = ':seat';
+    ";
+    // prepare the stament
+    $stmt = $conn->prepare($query);
+    // bind parameters
+    $stmt->bindParam(':flight_id', $flight_id, PDO::PARAM_STR);
+    $stmt->bindParam(':seat', $seat, PDO::PARAM_STR);
+
+    // TODO maybe fetch num
+    $result = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+    if (count($result) === 0) return false;
+
+    return true;
+}
 ?>
