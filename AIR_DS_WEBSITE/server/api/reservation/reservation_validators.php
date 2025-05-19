af9863f0bd1191efea540e0d14c67cf734271896
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-19 18:18:24 +0300

    added validator for name

--- AIR_DS_WEBSITE/server/api/reservation/reservation_validators.php
+++ AIR_DS_WEBSITE/server/api/reservation/reservation_validators.php
@@ -27,7 +27,6 @@ function is_airport_code_valid($conn, $code, $response) {
     return $response['success'];
 }
 
-
 // 0: Object { name: "Athens International Airport 'Eleftherios Venizelos'", code: "ATH" }
 // ​
 // 1: Object { name: "Brussels Airport", code: "BRU" }
@@ -40,4 +39,26 @@ function is_airport_code_valid($conn, $code, $response) {
 // ​
 // 5: Object { name: "Adolfo Suárez Madrid–Barajas Airport", code: "MAD" }
 
+
+/**
+ * Summary of is_name_valid
+ * 
+ * function that checks the validity of a name
+ * 
+ * This function is used to check the validity of both name and surname
+ * 
+ * a name is valid if:
+ *  - it not empty or null
+ *  - it contains only letters
+ *  - it has no more than 20 and no less than 3 characters
+ * 
+ * @param mixed $name - the name to be validated
+ * @param mixed $response - an array containing response messages
+ */
+function is_name_valid ($name, $response) {
+    if(!isset($name) || empty($name)) return $response['name']['missing'];
+    if(!is_only_letters($name)) return $response['name']['invalid'];
+    if (strlen($name) < 3 || strlen($name) > 20) return $response['name']['invalid'];
+    return $response['success'];
+}
 ?>
