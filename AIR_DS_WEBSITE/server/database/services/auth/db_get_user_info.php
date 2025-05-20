405ac872f4914db4998142dddb85d698c9261604
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-21 00:15:08 +0300

    added function that returns users name and surname given their username

--- AIR_DS_WEBSITE/server/database/services/auth/db_get_user_info.php
+++ AIR_DS_WEBSITE/server/database/services/auth/db_get_user_info.php
@@ -1,9 +1,9 @@
 <?php
 // this file has functions to details aboput the user of an account
 
-db_get_full_name($conn, $username) {
+function db_get_full_name($conn, $username=null) {
     if ($username === null) 
-        throw InvalidArgumentException ("Username must not be null");
+        throw new InvalidArgumentException ("Username must not be null");
 
     $query = 
     "   SELECT
@@ -15,10 +15,11 @@ db_get_full_name($conn, $username) {
     ";
     $stmt = $conn->prepare($query);
     $stmt->bindParam(":username", $username, PDO::PARAM_STR);
+    $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     // result contains all the rows (1 row of the executed query)
     // get the 1st row
-    return $resutl[0];
+    return $result[0];
 }
 ?>
