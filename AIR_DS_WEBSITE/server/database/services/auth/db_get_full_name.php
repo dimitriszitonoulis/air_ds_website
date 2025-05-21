d495b919b6387a3760a1e3674b055cbbfc8c1c8d
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-21 11:09:41 +0300

    renamed

---
+++ AIR_DS_WEBSITE/server/database/services/auth/db_get_full_name.php
@@ -0,0 +1,26 @@
+<?php
+// this file has functions to details aboput the user of an account
+
+function db_get_full_name($conn, $username=null) {
+    if ($username === null) 
+        throw new InvalidArgumentException ("Username must not be null");
+
+
+    $query = 
+    "   SELECT
+            name, surname
+        FROM
+            users
+        WHERE
+            username = :username;
+    ";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":username", $username, PDO::PARAM_STR);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // result contains all the rows (1 row of the executed query)
+    // get the 1st row
+    return $result[0];
+}
+?>
