421a5eaab756ae5442477175f60dc5d6b22e2bf1
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-22 22:12:04 +0300

    added function that returns airport information

--- AIR_DS_WEBSITE/server/database/services/reservations/db_get_flight_info.php
+++ AIR_DS_WEBSITE/server/database/services/reservations/db_get_flight_info.php
@@ -104,4 +104,36 @@ function db_get_taken_seats($conn, $dep_code=null, $dest_code=null, $dep_date=nu
     return $result;
 }
 
+function db_get_airport_information($conn, $dep_code = null, $dest_code = null) {
+     // if no departure airport code is provided
+    if ($dep_code === null)
+        throw new InvalidArgumentException("Departure airport code must not be null.");
+
+    // if no destination airport code is provided
+    if ($dest_code === null)
+        throw new InvalidArgumentException("Destination airport code must not be null.");
+
+
+    $query =
+    "   SELECT
+           latitude, longitude, fee
+        FROM 
+           airports 
+        WHERE 
+            code = :dep_code OR code = :dest_code;
+    ";
+    // prepare statement
+    $stmt = $conn->prepare($query);
+    // bind parameters
+    $stmt->bindParam(':dep_code', $dep_code, PDO::PARAM_STR);
+    $stmt->bindParam(':dest_code', $dest_code, PDO::PARAM_STR);
+    // execute query
+    $stmt->execute();
+
+    // retrieve response (first fetched column)
+    $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $response;
+}
+
 ?>
