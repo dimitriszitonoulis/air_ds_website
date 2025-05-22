219fe7db18c475aedcd9620b7f5c05c752d93545
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-22 12:51:47 +0300

    added file that contains scripts for getting the booking info

---
+++ AIR_DS_WEBSITE/client/scripts/booking/getBookingInfo.js
@@ -0,0 +1,56 @@
+export async function getFullName(values, BASE_URL) {
+   const url = `${BASE_URL}server/api/auth/get_user_info.php`;
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: { 'Content-Type': "application/json"},
+            body: JSON.stringify(values)
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            console.error("Server returned error", data);
+            throw new Error("HTTP error " + response.status);
+        }
+
+        console.log("Fetch succesful return data:", data)
+        
+        return {'name': data['name'], 'surname': data['surname']}
+    } catch (error) {
+        console.error("Error fetching data: ", error);
+        return false;
+    }
+}
+
+export async function getTakenSeats (values, BASE_URL) {
+
+    const url = `${BASE_URL}server/api/reservation/get_taken_seats.php`;
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: { 'Content-Type': "application/json"},
+            body: JSON.stringify(values)
+        });
+
+        console.log(await response.text());
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            console.error("Server returned error", data);
+            throw new Error("HTTP error " + response.status);
+        }
+
+        console.log("Fetch succesful return data:", data)
+
+        // if the user is registered this is true, otherwise false
+        return data['result'];
+
+    } catch (error) {
+        console.error("Error fetching data: ", error);
+        return false;
+    }
+}
