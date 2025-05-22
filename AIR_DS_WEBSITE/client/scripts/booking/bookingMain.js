d1865bc9c274f050eb3bd68fe44c539dede9fd91
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-22 21:42:25 +0300

    added event listeners for the seats and  the passenger fieldsets

--- AIR_DS_WEBSITE/client/scripts/booking/bookingMain.js
+++ AIR_DS_WEBSITE/client/scripts/booking/bookingMain.js
@@ -37,7 +37,7 @@ const values = {
     "dep_code": DEPARTURE_AIRPORT,
     "dest_code": DESTINATION_AIRPORT,
     "dep_date": DATE
-}
+};
 let takenSeats = await getTakenSeats(values, BASE_URL);     // get taken seats 
 createSeatMap(takenSeats);                                  // pass them to seat map function 
 const planeBody = document.getElementById('plane-body');    // get the plane body div
@@ -49,7 +49,7 @@ const seatmapContainer = document.getElementById('seat-map-container');
 
 
 // ------------------------------------------------------------------------------
-//                  ADD INFO ABOUT THE REST OF THE PEOPLE
+//                      ADD INFO ABOUT THE REST OF THE PEOPLE
 
 // add  fielsets for the rest of the users
 addInfoFieldSets(TICKET_NUMBER);
@@ -80,20 +80,6 @@ validateRealTime(bookingFields);
 const chooseSeatsBtn = document.getElementById('choose-seats-button');
 const chooseSeatsErrorDiv = document.getElementById('choose-seats-button-error-message');
 
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// TODO delete later
-
-//  const values = {
-//             "dep_code": DEPARTURE_AIRPORT,
-//             "dest_code": DESTINATION_AIRPORT,
-//             "dep_date": DATE
-//         }
-
-//         let takenSeats = await getTakenSeats(values, BASE_URL);
-
-//         createSeatMap(takenSeats);
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 let isAllValid = false;
 isAllValid = true;
@@ -119,9 +105,91 @@ chooseSeatsBtn.addEventListener('click', async (e) => {
 
 })
 
+//-----------------------------------------------------------------------------------
+//                                   SELECT SEAT CODE
+// the fielset of the currenly selected passenger
+let curSeatDiv = null;
+let selectedSeats = [];
 
 //TODO maybe only add the event listeners if the validation has succeded
+// use query selector to be able to user for each afterwards
+const passengerFieldsets = document.querySelectorAll(".passenger-info");
+passengerFieldsets.forEach((curFieldset) => 
+    curFieldset.addEventListener('click', (e) => {
 
+        // if the current fieldset is already selected de-select it 
+        if (curSeatDiv === curFieldset.querySelector(".seat-info")) {
+            curFieldset.style.backgroundColor = "";
+            curSeatDiv = null;
+            // curUserId = null;
+            return;
+        }
+
+        // TODO when the last information button is clicked for the price  
+        // do not forget to clear all the green colors 
+
+        // if the current fieldset is pressed remove the colors from the others
+        passengerFieldsets.forEach((fieldset) => fieldset.style.backgroundColor = "");
+        
+        // make current fieldset green
+        curFieldset.style.backgroundColor = "#93C572";  
+        // store the seat info div that is child of the current fieldset
+        curSeatDiv = curFieldset.querySelector(".seat-info");
+
+        // curUserId = curFieldset
+        // console.log(curUserId);
+
+    })
+);
+
+//select all the seats inside the seat map
+const seats = planeBody.querySelectorAll(".seat");
+seats.forEach((seat) => 
+    seat.addEventListener('click', (e) => {
+
+
+        // if no passengers are selected nop
+        // ONLY when a passenger is selected, can a seat be selected
+        if (curSeatDiv === null) return;
+
+        // if the user tries to select a seat for a specific person,
+        // and they click 2 times on the same seat, de-select the seat
+        if (curSeatDiv.innerText === seat.id) {
+            seat.style.backgroundColor = "";
+            curSeatDiv.innerText = "--";
+            const i = selectedSeats.indexOf(seat.id);   // find index of element to be removed
+            selectedSeats.splice(i, 1);                 // remove 1 element at the selected index
+            return;
+        }
+
+        // if a seat is already selected, and the user tries to select another seat
+        // without deselecting first, do nothing
+
+        // if the seat for the current passenger already has a value, 
+        // and the same passenger tries to select another seat, do not let them
+        if (curSeatDiv.innerText !== "--") {
+            return;
+        }
+        
+
+        // if a seat is selected by another passenger,
+        // do not re-select it
+        if (selectedSeats.includes(seat.id)) {
+            console.log("already included");
+            return;
+        }
+
+
+        seat.style.backgroundColor = "#93C572";
+        curSeatDiv.innerText = seat.id;
+        selectedSeats.push(seat.id);
+        curSeatDiv = null;     // otherwise 1 passenger can choose multiple seats
+
+        console.log(selectedSeats);
+    })
+);
+
+//-----------------------------------------------------------------------------------
 
 
 
