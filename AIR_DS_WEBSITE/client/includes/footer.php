2fc6e06d7bbb5e37c9e5456933023d242b28e289
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-24 23:24:46 +0300

    added iframe with map

--- AIR_DS_WEBSITE/client/includes/footer.php
+++ AIR_DS_WEBSITE/client/includes/footer.php
@@ -1,13 +1,21 @@
 <footer id="footer">
-  We wish you a happy trip!
 
   <div id="about-us">
     <a href="tel:+302102103330">mobile number</a>
     <a href="mailto:air_ds@unipi.gr">email</a>
-    Copyright &copy; 2025 Air Ds. All Rights Reserved
+    <span>Copyright &copy; 2025 Air Ds.</span>
+    <span>All Rights Reserved</span>
+     
   </div>
   <div id="map">
-    A map
+   <iframe src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=%CE%91%CE%B5%CF%81%CE%BF%CE%B4%CF%81%CF%8C%CE%BC%CE%B9%CE%BF%20%CE%95%CE%BB%CE%B5%CF%85%CE%B8%CE%BB%CE%B5%CF%81%CE%B9%CE%BF%CF%82+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+      <a href="https://www.gps.ie/collections/personal-trackers/"></a>
+    </iframe>
   </div>
+
+
+
+
+
 
 </footer>
