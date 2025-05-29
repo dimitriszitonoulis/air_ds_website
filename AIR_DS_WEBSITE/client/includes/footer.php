23679bb1c9b7beead1e57da648be78209cdf6d39
Dim <188760755+Dim607@users.noreply.github.com>
2025-05-29 23:30:32 +0300

    changed iframe, added  the one from google maps

--- AIR_DS_WEBSITE/client/includes/footer.php
+++ AIR_DS_WEBSITE/client/includes/footer.php
@@ -8,14 +8,15 @@
      
   </div>
   <div id="map">
-   <iframe src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=%CE%91%CE%B5%CF%81%CE%BF%CE%B4%CF%81%CF%8C%CE%BC%CE%B9%CE%BF%20%CE%95%CE%BB%CE%B5%CF%85%CE%B8%CE%BB%CE%B5%CF%81%CE%B9%CE%BF%CF%82+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-      <a href="https://www.gps.ie/collections/personal-trackers/"></a>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3146.5234454282404!2d23.946440764882503!3d37.94156215785798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1901ad9e75c61%3A0x38b215df0aeeb3aa!2sAthens%20International%20Airport%20%22Eleftherios%20Venizelos%22!5e0!3m2!1sen!2sgr!4v1748550338821!5m2!1sen!2sgr" 
+      width="400" 
+      height="300" 
+      style="border:0;"
+      allowfullscreen="" 
+      loading="lazy" 
+      referrerpolicy="no-referrer-when-downgrade">
     </iframe>
+
   </div>
-
-
-
-
-
 
 </footer>
