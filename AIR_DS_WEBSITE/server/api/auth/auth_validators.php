b3f97cfac978a7ea7f9bd97dd0f752ed2ec61a15
Dim <188760755+Dim607@users.noreply.github.com>
2025-06-01 14:50:01 +0300

    updated documentation

--- AIR_DS_WEBSITE/server/api/auth/auth_validators.php
+++ AIR_DS_WEBSITE/server/api/auth/auth_validators.php
@@ -54,6 +54,24 @@ function get_syntax_validators() {
     ];
 }
 
+/**
+ * Summary of is_name_valid
+ * 
+ * This function performs validation for a given name
+ * 
+ * A name is considered valid if:
+ *  - It is set and not empty
+ *  - It only contains characters
+ * 
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *                                      ATTENTION
+ * The same function is used for the validation of the surname
+ * 
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * @param string $name - the name to be validated
+ * @param mixed $response - array containing response messages sepcified in config/messages.php
+ * @return mixed - the appropriate response message
+ */
 function is_name_valid ($name, $response) {
     if(!isset($name) || empty($name)) return $response['name']['missing'];
     if(!is_only_letters($name)) return $response['name']['invalid'];
@@ -68,9 +86,9 @@ function is_name_valid ($name, $response) {
  * - It has correct syntax
  * - It does not already exist in the database (there is no other user with that username)
  * 
- * @param mixed $conn - the connection to the db
- * @param mixed $username
- * @param mixed $response - an array containing response messages
+ * @param PDO $conn - the connection to the db
+ * @param string $username - the username to be validated
+ * @param mixed $response - array containing response messages
  */
 function is_username_valid($conn, $username, $response) {
     $is_syntax = is_username_syntax_valid($username, $response);
@@ -88,11 +106,12 @@ function is_username_valid($conn, $username, $response) {
  * 
  * Checks if the supplied username has valid syntax 
  * A username is has valid syntax if:
- * - It is set and it is not null
+ * - It is set and not empty
  * - It only contains alphanumeric characters
  * 
- * @param mixed $username
+ * @param string $username - the username to be validated
  * @param mixed $response - an array containing response messages
+ * @return mixed - return the appropriate response message
  */
 function is_username_syntax_valid($username, $response) {
     if (!isset($username) || empty($username)) return $response['username']['missing'];
@@ -100,6 +119,16 @@ function is_username_syntax_valid($username, $response) {
     return $response['success'];
 }
 
+/**
+ * Summary of is_username_stored
+ * 
+ * Checks if a username is stored in the database
+ * 
+ * @param PDO $conn - the connection to the database
+ * @param string $username - the username to be validated
+ * @param mixed $response - array containing response messages
+ * @return mixed - the appropriate response message
+ */
 function is_username_stored($conn, $username, $response) {
     $is_stored = false;
     // check if the username is taken
@@ -115,7 +144,20 @@ function is_username_stored($conn, $username, $response) {
     return $response['success'];
 }
 
-
+/**
+ * Summary of is_password_syntax_valid
+ * 
+ * Checks if the syntax of the supplied password is valid
+ * 
+ * A password has valid syntax if:
+ * - It is set and not empty
+ * - It contains at least one number
+ * - Its length is between 4 and 10 digits
+ * 
+ * @param string $password - the password to be validated
+ * @param mixed $response - array containing response messages
+ * @return  - the appropriate response message
+ */
 function is_password_syntax_valid ($password, $response) {
     if (!isset($password) || empty($password)) return $response['password']['missing'];
     if (!contains_number($password)) return $response['password']['invalid'];
@@ -123,6 +165,21 @@ function is_password_syntax_valid ($password, $response) {
     return $response['success'];
 }
 
+/**
+ * Summary of is_email_valid
+ * 
+ * Checks it the supplied email is valid
+ * 
+ * An email is valid if: 
+ * - It is set and not empty
+ * - It is considered valid by filter_var as FILTER_VALIDATE_EMAIL 
+ * - It is not already stored in the database
+ * 
+ * @param PDO $conn - the connection to the database
+ * @param string $email - the email to be validated
+ * @param mixed $response - array containing response messages
+ * @return mixed - the appropriate response
+ */
 function is_email_valid ($conn, $email, $response) {
     if (!isset($email) || empty($email)) return $response['email']['missing'];
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) return $response['email']['invalid'];
@@ -133,7 +190,6 @@ function is_email_valid ($conn, $email, $response) {
         $is_stored= db_is_email_stored($conn, $email);
     } catch (Exception $e) {
         return $response['failure']['nop'];
-
     }
     // is there another account with that email?
     if ($is_stored) return $response['register']['email_taken'];
