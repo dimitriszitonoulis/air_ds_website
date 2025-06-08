9b83b13a5396de82ae7619c99eec69ffae89c032
Dim <188760755+Dim607@users.noreply.github.com>
2025-06-08 12:07:41 +0300

    added documentation for date validation and made it check if the date is 30 days after today

--- AIR_DS_WEBSITE/server/api/trips/trip_validators.php
+++ AIR_DS_WEBSITE/server/api/trips/trip_validators.php
@@ -16,7 +16,7 @@ require_once BASE_PATH . "server/api/reservation/reservation_validators.php";
  *  dest: (callable(mixed ):bool), 
  * }
  */
-function get_validators() {
+function get_validators_trip() {
     return [
         "dep_code" => function ($params)  {
             return is_airport_code_valid($params["conn"], $params["dep_code"], $params['response']); 
@@ -25,7 +25,7 @@ function get_validators() {
             return is_airport_code_valid($params["conn"], $params["dest_code"], $params['response']); 
         },
         "dep_date" => function ($params) {
-            return is_departure_date_valid($params["conn"], $params["dep_code"], $params['dest_code'], $params['dep_date'], $params['response']);
+            return is_departure_date_valid_cancelation($params["conn"], $params["dep_code"], $params['dest_code'], $params['dep_date'], $params['response']);
         },
         "username" => function($params) {
             return is_username_valid_booking($params["conn"], $params["username"], $params["response"]);
@@ -34,16 +34,34 @@ function get_validators() {
 }
 
 
-//TODO check that date is 30 days after current date before cancelation
+
+/**
+ * Summary of is_departure_date_valid_cancelation
+ * 
+ * Check if the departure date for a flight is valid when a user wants to cancel the flight
+ * 
+ * A flight is considered valid if:
+ * - It is at least 30 days after the current date
+ * - There is a flight between the specified airports for that date
+ * 
+ * @param PDO $conn
+ * @param string $dep_code - The code of the departure airport
+ * @param string $dest_code - The code of the destination airport
+ * @param string $dep_date - the departure date
+ * @param mixed $response - An array containing response messages
+ * @return mixed - the appropriate response message
+ */
 function is_departure_date_valid_cancelation($conn, $dep_code, $dest_code, $dep_date, $response) {
     $today = date("Y-m-d");
     
     // get current date and departure date in seconds
     $today_sec = strtotime($today);
     $dep_date_sec = strtotime($dep_date);
+    $month_seconds = 30 * 86400;
 
     $difference = $dep_date_sec - $today_sec;
-    if ($difference <= 0)  return $response['dep_date']['invalid'];
+    // is the dep_date at least 30 days into the future
+    if ($difference < $month_seconds) return $response['dep_date']['invalid'];
 
     $is_stored = db_is_date_stored($conn, $dep_code, $dest_code, $dep_date);
     if (!$is_stored) return $response['dep_date']['invalid'];
