6a8948ebd9ccbb300f1cec0a8d2217f2e12fa98b
Dim <188760755+Dim607@users.noreply.github.com>
2025-06-08 19:22:43 +0300

    added script that books tickets

---
+++ AIR_DS_WEBSITE/client/scripts/booking/bookTickets.js
@@ -0,0 +1,28 @@
+export async function bookTickets(values, BASE_URL) {
+    let url = `${BASE_URL}server/api/reservation/book_tickets.php`;
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: { 'Content-Type': "application/json"},
+            body: JSON.stringify(values)
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            console.error("Server returned error", data);
+            throw new Error("HTTP error " + response.status);
+        }
+
+
+        console.log("Fetch succesful return data:", data)
+
+        // if the user is registered this is true, otherwise false
+        return data['result'];
+
+    } catch (error) {
+        console.error("Error fetching data: ", error);
+        return false;
+    }
+}
