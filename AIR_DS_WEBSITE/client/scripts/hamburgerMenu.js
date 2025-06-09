112db28bdb8f88bd80dd144cf1f2b97ceed82c1d
Dim <188760755+Dim607@users.noreply.github.com>
2025-06-09 20:42:07 +0300

    added hamurger menu script

---
+++ AIR_DS_WEBSITE/client/scripts/hamburgerMenu.js
@@ -0,0 +1,18 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+    hamburger.style.display = "none";
+    nav.style.display = "flex";
+});
+
+document.addEventListener("click", (e) => {
+    if (window.innerWidth <= 800 && nav.style.display === 'flex'
+        && e.target !== hamburger
+    ) {
+        hamburger.style.display = "block";
+        nav.style.display = "none";
+    }
+
+
+})
