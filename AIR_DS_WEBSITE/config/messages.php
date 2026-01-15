f922f521bdd367ea266a3ebb196584cc1477da38
Dim <188760755+Dim607@users.noreply.github.com>
2026-01-15 12:13:18 +0200

    removed functions that were not referenced anywhere

--- AIR_DS_WEBSITE/config/messages.php
+++ AIR_DS_WEBSITE/config/messages.php
@@ -50,29 +50,3 @@ function get_response_message($fields){
     }
     return $response_message;
 }
-
-// TODO delete function
-function response_register($fields){
-    foreach ($fields as $field) {
-        $response_message[$field] = [
-            "missing" => ["result" => false, "message" => "missing field: $field"],
-            "failure" => ["result" => false, "message" => "invalid $field"],
-            "success" => ["result" => true, "message" => "valid $field"]
-        ];
-    }
-}
-
-// TODO delete function
-function add_field_messages($response_message, $fields)
-{
-    // $fields = ["name", "surname", "username", "password", "email"];
-    foreach ($fields as $field) {
-        $response_message[$field] = [
-            "missing" => ["result" => false, "message" => "missing field: $field"],
-            "failure" => ["result" => false, "message" => "invalid $field"],
-            "success" => ["result" => true, "message" => "valid $field"]
-        ];
-    }
-    return $response_message;
-}
-?>
